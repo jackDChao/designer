4f8299bded1a459e4ce56fdbe5d8e59004d08156
jackDchao <ujjack@sina.com>
2020-08-24 21:34:03 +0800

    优化界面

--- js/extra/lessonindex.js
+++ js/extra/lessonindex.js
@@ -11,9 +11,15 @@ $(function(){
     getcourse()
     // 登录
     $(document).on('click','.login',function(){
-        location.href = `./login.html`
+        $('.mainCon').show()    
+        return false
+    }); 
+    // 关闭登录
+    $(document).on('click','.close_btn',function(){
+        $('.mainCon').hide()
         return false
     });
+    
     // 课程指向
     $(document).on('click','.my_lesson',function(){
         location.href = `./mycourse.html`
@@ -32,6 +38,30 @@ $(function(){
         if (r != null) return unescape(r[2]);
         return null;
     }
+    $('.layui-input').bind('input propertychange', function() {
+        if($(this).val()){
+            $(this).parent().find('.leftLabel').hide()
+        }else{
+            $(this).parent().find('.leftLabel').show()
+        }
+    })
+    $('.login_btn').click(function(){
+        let uname = $('.login_username').val()
+        let upwd = $('.login_password').val()
+        if(uname == 'admin' && upwd == 'p@ssword9870'){
+            layer.msg('登陆成功')
+            let user = {
+                name:'admin'
+            }
+            localStorage.setItem('user',JSON.stringify(user))
+            console.log(localStorage.getItem('user'))
+            setTimeout(() => {
+                location.reload()
+            }, 1500);
+        }else{
+            layer.msg('登陆失败，请重试')
+        }
+    })
 
     function getcourse(){
         $(".courselist").html('')
